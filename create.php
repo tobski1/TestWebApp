51d9096322ce7812aab19c66cfcd7f57bfc9f392
tobski1 <35900189+tobski1@users.noreply.github.com>
2018-04-30 12:59:46 +0100

    Spelling mistake in serverName

--- create.php
+++ create.php
@@ -9,11 +9,26 @@
  </head>
  <body>
  <?php
-  try {
-  $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "tobymysqlserver.mysql.database.azure.com", "toby@tobymysqlserver", "TheOffice1!!", "quickstartdb", 3306);
 
-  $query = "SELECT * FROM inventory";
-  echo $query;
+<?php
+$host = 'tobymysqlserver.mysql.database.azure.com';
+$username = 'toby@tobymysqlserver';
+$password = 'TheOffice1!!';
+$db_name = 'quickstartdb';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+//  try {
+//  $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "tobymysqlserver.mysql.database.azure.com", "toby@tobymysqlserver", "TheOffice1!!", "quickstartdb", 3306);
+//
+$query = "SELECT * FROM inventory";
+echo $query;
  ?>
 
 
